25abdd15dfca126efe340effc0c85088287d3581
Yaosquared <marioyaoyao14@gmail.com>
2024-10-27 20:31:07 +0800

    fix: disable initial system preference for bg mode and set default to light mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,6 @@ export default function RootLayout({
         <ThemeProvider
           attribute="class"
           defaultTheme="light"
-          enableSystem
           disableTransitionOnChange
         >
           {children}
