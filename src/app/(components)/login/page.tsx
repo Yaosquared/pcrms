20f7c137b72f367436883494a1d7669d8a0df6c1
Yaosquared <marioyaoyao14@gmail.com>
2024-10-30 17:07:42 +0800

    feat: enhanced login form UI for better user experience

--- src/app/(components)/login/page.tsx
+++ src/app/(components)/login/page.tsx
@@ -11,9 +11,9 @@ export default function LogIn() {
   const [showPassword, setShowPassword] = useState(false);
 
   return (
-    <div className="w-screen h-screen flex flex-col justify-center items-center text-center text-xl space-y-4">
-      <form className="border rounded-md shadow-md w-[80%] md:w-[60%] lg:w-[40%] 2xl:w-[30%] h-[40%] flex flex-col justify-center items-center space-y-4 bg-accent/60 dark:bg-accent/10 text-foreground text-xs lg:text-sm 2xl:text-base">
-        <h2 className="font-semibold text-base lg:text-lg 2xl:text-xl">
+    <div className="w-screen h-screen flex flex-col justify-center items-center text-center text-xl space-y-">
+      <form className="border rounded-md shadow-md w-[80%] md:w-[50%] lg:w-[40%] 2xl:w-[30%] h-[40%] md:h-[50%] 2xl:h-[60%] flex flex-col justify-center items-center space-y-4 bg-accent/60 backdrop-blur-lg dark:bg-accent/10 text-foreground text-xs lg:text-sm 2xl:text-base">
+        <h2 className="font-bold text-xl md:text-2xl lg:text-3xl 2xl:text-4xl lg:mb-2 xl:mb-4 2xl:mb-6 bg-gradient-to-r from-blue-500 to-blue-600 bg-clip-text text-transparent">
           PCRMS
         </h2>
         <input
@@ -21,9 +21,9 @@ export default function LogIn() {
           placeholder="Username"
           value={username}
           onChange={(e) => setUsername(e.target.value)}
-          className="h-10 w-[80%] px-3 py-2 border rounded-md placeholder:text-muted-foreground focus:outline-blue-600"
+          className="h-10 w-[70%] px-3 py-2 border rounded-md placeholder:text-muted-foreground focus:outline-blue-600"
         />
-        <div className="w-[80%] relative">
+        <div className="w-[70%] relative">
           <input
             type={showPassword ? "text" : "password"}
             placeholder="Password"
@@ -41,10 +41,14 @@ export default function LogIn() {
         </div>
         <Link
           href="/dashboard"
-          className="flex justify-center items-center w-[80%] sm:w-[50%] md:w-[33.33%] lg:w-[25%] xl:w-[20%] bg-blue-600 hover:bg-blue-700 text-white rounded-md h-10 sm:h-9 lg:h-11"
+          className="flex justify-center items-center w-[70%] bg-blue-600 hover:bg-blue-700 text-white rounded-md h-10 sm:h-9 lg:h-11"
         >
           Log In
         </Link>
+        <div className="w-[70%] flex flex-row justify-between text-xs 2xl:text-sm font-medium cursor-pointer">
+          <p className="hover:text-blue-700">Forgot Password?</p>
+          <p className="hover:text-blue-700 hover:underline">Sign up</p>
+        </div>
       </form>
     </div>
   );
